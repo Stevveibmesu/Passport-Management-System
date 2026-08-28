42f0660b926f531990dd54be93ed10eb8e73fe32
Steve Musembi <steveibmesum@gmail.com>
2026-08-28 16:03:44 +0300

    added changes

--- passport_system/settings.py
+++ passport_system/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'accounts.middleware.NoCacheMiddleware',
 ]
 
 ROOT_URLCONF = 'passport_system.urls'
